911f3cbf5abff951b40bfdb8f52671d78b2edb60
Olha Romanova <larklarky@gmail.com>
2020-05-19 16:02:45 +0200

    Updates

---
+++ precache-manifest.458b6d59a8a844f593fd089828149b90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893141b9b3df954a47c29255ac3ebad3",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f7f9b4fe0a56706a7a23",
+    "url": "/todo/static/css/main.07a33839.chunk.css"
+  },
+  {
+    "revision": "0f85c088161e501a88df",
+    "url": "/todo/static/js/2.f1f61109.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/todo/static/js/2.f1f61109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f9b4fe0a56706a7a23",
+    "url": "/todo/static/js/main.a7f6fefe.chunk.js"
+  },
+  {
+    "revision": "0fad5e3480eda5bbef65",
+    "url": "/todo/static/js/runtime-main.64f6f58d.js"
+  }
+]);
